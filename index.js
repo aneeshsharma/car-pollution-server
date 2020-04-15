8eed6c29be2ae53f05254744c75eaa8080537f70
Anish Sharma <aneeshsharma15@outlook.com>
2020-04-15 15:21:04 +0530

    Fixed port number

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send("Hello world"))
 
-app.listen(5000)
+app.listen(port, () => console.log("Started on port: ${port}"))
 
