1b64b99c1947bd5839c6e153be684b0a4c6f297e
Anish Sharma <aneeshsharma15@outlook.com>
2020-04-17 13:03:37 +0530

    Added getdata and recorddata routes

---
+++ crash.js
@@ -0,0 +1,2 @@
+var date = new Date("2019-03-22T02:00:00Z");
+console.log("${date}");
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 var MongoClient = require("mongodb").MongoClient;
 const conn = require("./config/deployement-config").conn;
 
 let db, dbo;
 
+console.log("Env URI: " + process.env.MONGO_URI);
 console.log("Connection URI : " + conn.uri);
 
 MongoClient.connect(conn.uri, (err, db_) => {
@@ -19,17 +21,90 @@ MongoClient.connect(conn.uri, (err, db_) => {
     });
 });
 
-app.get("/", (req, res) => res.send("Hello world"));
+function getCollection(deviceID) {
+    "pollution_data_" + deviceID;
+}
+
+app.use(
+    cors({
+        origin: "http://localhost",
+    })
+);
+
+app.get("/", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({ status: "INVALID" }));
+});
 
 app.get("/getdata", (req, res) => {
     res.setHeader("Content-Type", "application/json");
+    const deviceID = req.deviceID;
+    const startDate = new Date(req.start).toISOString;
+    const endDate = new Date(req.endDate).toISOString;
+
+    const query = {
+        startTime: { $lte: endDate },
+        endTime: { $gte: startDate },
+    };
+
+    const collectionName = getCollection(deviceID);
+
+    let journeyData;
+
+    db.collectionName(collectionName, (err, names) => {
+        if (err) {
+            res.send({
+                status: "FAILED",
+                err: "UNEXPECTED",
+            });
+        }
+        if (names.length <= 0) {
+            console.log(collectionName + " doesn't exist!");
+            res.send({
+                status: "FAILED",
+                err: "NO COLLECTION",
+            });
+            return;
+        }
+    });
+
+    dbo.collection(collectionName)
+        .find(query)
+        .toArray((err, res) => {
+            if (err) {
+                res.send({
+                    status: "FAILED",
+                    err: "UNEXPECTED",
+                });
+            }
+            console.log("Got result " + res);
+            journeyData = res;
+        });
+
     const result = {
         status: "SUCCESS",
-        data: {
-            co2_content: 100,
-        },
+        journeyData: journeyData,
     };
+
+    res.setHeader("Content-Type", "application/json");
     res.send(JSON.stringify(result));
+});
+
+app.get("/recorddata", (req, res) => {
+    const deviceID = req.deviceID;
+    const journeyData = req.journeyData;
+    const collectionName = getCollection(deviceID);
+
+    dbo.createCollection(collectionName);
+    dbo.collection(collectionName).insertOne(journeyData, (err, res) => {
+        if (err) throw err;
+        console.log("Joruney Data inserted");
+    });
+
+    res.setHeader("Content-Type", "application/json");
+    res.send({
+        status: "SUCCESS",
+    });
 });
 
 app.listen(port, () => console.log("Started on port: " + port));
